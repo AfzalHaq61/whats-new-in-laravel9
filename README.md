7ca7217f04433998491ddf5240480667fb5161fb
AfzalHaq61 <afzaldev6@gmail.com>
2023-10-20 18:56:29 +0500

    Controller Route Groups
    you can group routes by controller like this
    Route::controller(PostController::class)->group(function() {
        Route::get('/posts', 'index');
        Route::post('/posts', 'store');
        Route::get('/posts/{post}', 'show');
    });

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # whats-new-in-laravel9
+
+1-Video (Controller Route Groups)
+
+you can group routes by controller like this
+Route::controller(PostController::class)->group(function() {
+    Route::get('/posts', 'index');
+    Route::post('/posts', 'store');
+    Route::get('/posts/{post}', 'show');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(PostController::class)->group(function() {
+    Route::get('/posts', 'index');
+    Route::post('/posts', 'store');
+    Route::get('/posts/{post}', 'show');
+});
+
